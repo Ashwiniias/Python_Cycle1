1104989ffccee98fe347939380fd88b85bab2124
Dharmavarapu Ashwini <dharmavarapuashwini@gmail.com>
2026-07-02 12:26:57 +0530

    Add files via upload

---
+++ Day-1-variables.py
@@ -0,0 +1,50 @@
+#store and print
+name = "Ashwini"
+age = 20
+branch = "CSE"
+print(name)
+print(age)
+print(branch)
+
+
+#change value and print 
+age = 20
+print(age)
+age = 21
+print(age)
+
+
+#Assigning Multiple Variables
+a = b = c = 100
+print(a)
+print(b)
+print(c)
+
+
+#Assigning Multiple Values
+name, age, branch = "Ashwini", 20, "CSE"
+print(name)
+print(age)
+print(branch)
+
+
+#Swapping Variables
+a = 10
+b = 20
+a, b = b, a
+print(a)
+print(b)
+
+
+#Store your name and print it.
+Name = "Ashwini"
+print(Name)
+
+
+#Store your age and print it.
+age = 20
+print(age)
+
+
+#Store your college name.
+College = "AVNIET"
